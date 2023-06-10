11da633ecd1172e8403d43f70ef9dc50269ea045
Ana Lopez <darkanita@gmail.com>
2023-06-10 13:40:25 -0500

    Adding model definition

---
+++ src/model/src/AlexNet.py
@@ -0,0 +1,43 @@
+from math import floor
+
+import torch.nn as nn
+
+class AlexNet(nn.Module):
+    def __init__(self, 
+                 num_classes,
+                 input_shape,
+                 hidden_layer_sizes=[32, 64],
+                 kernel_sizes=[3],
+                 activation="ReLU",
+                 pool_sizes=[2],
+                 dropout=0.5):
+      
+        super(AlexNet, self).__init__()
+
+        self.layer1 = nn.Sequential(
+              nn.Conv2d(in_channels=input_shape[0], out_channels=hidden_layer_sizes[0], kernel_size=kernel_sizes[0]),
+              getattr(nn, activation)(),
+              nn.MaxPool2d(kernel_size=pool_sizes[0])
+        )
+        self.layer2 = nn.Sequential(
+              nn.Conv2d(in_channels=hidden_layer_sizes[0], out_channels=hidden_layer_sizes[-1], kernel_size=kernel_sizes[-1]),
+              getattr(nn, activation)(),
+              nn.MaxPool2d(kernel_size=pool_sizes[-1])
+        )
+        self.layer3 = nn.Sequential(
+              nn.Flatten(),
+              nn.Dropout(dropout)
+        )
+
+        fc_input_dims = floor((input_shape[1] - kernel_sizes[0] + 1) / pool_sizes[0]) # layer 1 output size
+        fc_input_dims = floor((fc_input_dims - kernel_sizes[-1] + 1) / pool_sizes[-1]) # layer 2 output size
+        fc_input_dims = fc_input_dims*fc_input_dims*hidden_layer_sizes[-1] # layer 3 output size
+
+        self.fc = nn.Linear(fc_input_dims, num_classes)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.fc(x)
+        return x
