26a358aa1fd24f68d2e03a5f2bd487e3c2983f36
Ana Lopez <darkanita@gmail.com>
2023-06-11 08:57:04 -0500

    Change model to use

--- src/model/build.py
+++ src/model/build.py
@@ -1,8 +1,7 @@
 import torch
 
 # Import the model class from the main file
-from src.MLP import MLP
-from src.AlexNet import AlexNet
+from src.Classifier import Classifier
 
 import os
 import argparse
@@ -17,10 +16,6 @@ if args.IdExecution:
 else:
     args.IdExecution = "testing console"
 
-
-
-import os
-
 # Check if the directory "./model" exists
 if not os.path.exists("./model"):
     # If it doesn't exist, create it
@@ -28,7 +23,7 @@ if not os.path.exists("./model"):
 
 # Data parameters
 num_classes = 10
-input_shape = (1, 28, 28)
+input_shape = 784
 
 def build_model_and_log(config, model, model_name="MLP", model_description="Simple MLP"):
     with wandb.init(project="MLOps-Pycon2023", name=f"initialize Model ExecId-{args.IdExecution}", job_type="initialize-model", config=config) as run:
@@ -49,15 +44,13 @@ def build_model_and_log(config, model, model_name="MLP", model_description="Simp
 
         run.log_artifact(model_artifact)
 
-model_config = {"num_classes":num_classes,
-                "input_shape":input_shape,
+
+# MLP
+model_config = {"input_shape":input_shape,
+                "num_classes":num_classes,
                 "hidden_layer_sizes": [32, 64],
-                "kernel_sizes": [3],
-                "activation": "ReLU",
-                "pool_sizes": [2],
-                "dropout": 0.5,
-                "num_classes": 10}
+                "activation": "ReLU"}
 
-model = AlexNet(**model_config)
+model = Classifier(input_shape, 32, 64, num_classes)
 
-build_model_and_log(model_config, model, "convnet","Simple AlexNet style CNN")
+build_model_and_log(model_config, model, "Linear","Simple Linear Classifier")
