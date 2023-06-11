1ac46590d1e9587230641f96155c5777622f6cef
Ana Lopez <darkanita@gmail.com>
2023-06-11 09:33:43 -0500

    Building model

--- src/model/src/Classifier.py
+++ src/model/src/Classifier.py
@@ -2,11 +2,11 @@ from torch import nn
 import torch.nn.functional as F
 
 class Classifier(nn.Module):
-  def __init__(self,D_in, H1, H2, D_out):
+  def __init__(self,input_shape, hidden_layer_1, hidden_layer_2, num_classes):
     super().__init__()
-    self.linear1 = nn.Linear(D_in,H1)
-    self.linear2 = nn.Linear(H1,H2)
-    self.linear3 = nn.Linear(H2,D_out)
+    self.linear1 = nn.Linear(input_shape,hidden_layer_1)
+    self.linear2 = nn.Linear(hidden_layer_1,hidden_layer_2)
+    self.linear3 = nn.Linear(hidden_layer_2,num_classes)
 
   def formard(self, x):
     x = F.relu(self.linear1(x))
